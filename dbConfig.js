c0eb3253a0782510be0bc2d8fa673519df249557
jerkerolvedal <144764241+jerkerolvedal@users.noreply.github.com>
2025-01-04 09:39:45 +0100

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,1 +1,10 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'your_username',
+    host: 'localhost',
+    database: 'your_database',
+    password: 'your_password',
+    port: 5432,
+});
 
+module.exports = pool;
